edc303145b7102e1fbe6a2ec6571cb9d2a4f2aa4
MtstU <78070630+MtstU@users.noreply.github.com>
2021-02-13 17:08:50 +0300

    created new StarShip printed height

--- first/src/main/java/FirstTest.java
+++ first/src/main/java/FirstTest.java
@@ -1,6 +1,18 @@
+import test.ship.StarShip;
+
 public class FirstTest {
 	
     public static void main (String[] args) {
-        System.out.println ("We are here! Hello!");
+        //создадим экземпляр класса корабля StarShip
+        StarShip ship = new StarShip();
+
+        //Зададим кораблю свойство-высоту
+        ship.shipHeight = 258.28;
+        displayHigh(ship);
+
     }
+    public static void displayHigh(StarShip ourShip) {
+        System.out.println("Высота корабля составляет : "+ourShip.shipHeight);
+    }
+
 }
