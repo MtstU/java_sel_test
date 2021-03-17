378ee31425c7fb26479613801a78734896a88e2f
MtstU <78070630+MtstU@users.noreply.github.com>
2021-03-17 14:50:54 +0300

    11

---
+++ first/src/main/java/JavaRush/OtherTasks.java
@@ -0,0 +1,122 @@
+public class OtherTasks {
+
+    /*
+
+     */
+    public static void main(String[] args) {
+        String p = " ";
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int e = 0;
+        int f = 0;
+        int g = 0;
+        int h = 0;
+        int j = 0;
+        int k = 0;
+        for (int i = 1; i <= 10; i++) {
+            a++;
+            b = b + 2;
+            c = c + 3;
+            d = d + 4;
+            e = e + 5;
+            f = f + 6;
+            g = g + 7;
+            h = h + 8;
+            j = j + 9;
+            k = k + 10;
+
+            System.out.print(a + p);
+
+            System.out.print(b + p);
+
+            System.out.print(c + p);
+
+            System.out.print(d + p);
+
+            System.out.print(e + p);
+
+            System.out.print(f + p);
+
+            System.out.print(g + p);
+
+            System.out.print(h + p);
+
+            System.out.print(j + p);
+
+            System.out.print(k + p);
+            System.out.println("");
+        }
+        System.out.println("");
+
+    }
+}
+
+       /* int b = 0;
+        for (int i = 1; i <= 10; i++ ) {
+            b = b+2;
+            System.out.print(b + p);
+        }
+        System.out.println("");
+
+
+        int c = 0;
+        for (int i = 1; i <= 10; i++ ) {
+            c = c+3;
+            System.out.print(c + p);
+        }
+        System.out.println("");
+
+        int d = 0;
+        for (int i = 1; i <= 10; i++ ) {
+            d = d+4;
+            System.out.print(d + p);
+        }
+        System.out.println("");
+
+        int e = 0;
+        for (int i = 1; i <= 10; i++ ) {
+            e = e+5;
+            System.out.print(e + p);
+        }
+        System.out.println("");
+
+        int g = 0;
+        for (int i = 1; i <= 10; i++ ) {
+            g = g+6;
+            System.out.print(g + p);
+        }
+        System.out.println("");
+
+        int h = 0;
+        for (int i = 1; i <= 10; i++ ) {
+            h = h+7;
+            System.out.print(h + p);
+        }
+        System.out.println("");
+
+        int j = 0;
+        for (int i = 1; i <= 10; i++ ) {
+            j = j+8;
+            System.out.print(j + p);
+        }
+        System.out.println("");
+
+        int k = 0;
+        for (int i = 1; i <= 10; i++ ) {
+            k = k+9;
+            System.out.print(k + p);
+        }
+        System.out.println("");
+
+        int l = 0;
+        for (int i = 1; i <= 10; i++ ) {
+            l = l+10;
+            System.out.print(l + p);
+        }
+        System.out.println("");
+    }
+} */
+
+
